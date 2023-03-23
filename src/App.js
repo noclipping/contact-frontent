82926b81d1dc938edac55359b18e551e34374732
noclipping <aresous@gmail.com>
2023-03-23 17:35:45 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [contacts, setContacts]=useState([]);
+
+  useEffect(()=>{
+    getData();
+  }, [])
+
+  function getData(){
+    fetch('http://localhost:3000/contacts')
+    .then(res=> res.json())
+    .then(res=>setContacts([...res]))
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          contacts.map(contact=>{
+            return(
+              <div className='contactCard' key={contact.id}>
+                <h3>{contact.name}</h3>
+                <p>{contact.email_address}</p>
+                <p>{contact.age}</p>
+              </div>
+            )
+          })
+        }
       </header>
     </div>
   );
